721e8f5bb1c0ad51562f4511906fa4fcefa21032
ManthanJadhav1809 <manthan1809@gmail.com>
2024-12-05 13:03:51 +0530

    second

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// http://localhost:5000/
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://user-data-mern-assement.vercel.app/",
 });
 
 api.interceptors.request.use((config) => {
