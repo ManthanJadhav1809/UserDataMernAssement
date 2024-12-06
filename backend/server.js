7f5a9a62f67e061c555f6bb66b9db5fd37882a4b
ManthanJadhav1809 <manthan1809@gmail.com>
2024-12-06 10:02:24 +0530

    ss

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,16 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGOURL;
 
 
-app.use(cors({ origin:"https://user-data-mern-assement-frontend.vercel.app/" }));
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://user-data-mern-assement-frontend.vercel.app",
+    methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
+    credentials: true, // If your frontend sends cookies or requires credentials
+  })
+);
+
+app.options("*", cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
